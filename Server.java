dc85ea0bf674401f5140b75f1c3b220ec1858953
ricardopuno <ricardopuno111@gmail.com>
2025-11-05 13:25:14 -0700

    prints out incorrect? numbers

--- Server.java
+++ Server.java
@@ -6,6 +6,31 @@ public class Server {
         // Peterson's
         System.out.println("CS Solution 1 – Peterson’s (time in milli-seconds)");
         System.out.println("Threads\tAVG TAT");
+
+        for (int numThreads = 2; numThreads <= 100; numThreads++) {
+            Peterson section = new Peterson(numThreads);
+            Worker[] workers = new Worker[numThreads];
+
+            for (int i = 0; i < numThreads; i++) {
+                workers[i] = new Worker(section, i);
+            }
+
+            for (Worker w : workers) w.start();
+            for (Worker w : workers) {
+                try {
+                    w.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            long total = 0;
+            for (Worker w : workers) total += w.turnaroundTime;
+            double avgTAT = (double) total / numThreads;
+
+            System.out.printf("%d\t%.5f%n", numThreads, avgTAT);
+
+        }
         
 
     }
